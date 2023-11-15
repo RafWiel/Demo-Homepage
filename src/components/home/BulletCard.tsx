3b87b66ff945b3028e6e9ccf9b2d2048b87fc688
rafal.wielicki <rafal.wielicki@gmail.com>
2023-11-15 09:21:20 +0100

    WIP Add BulletCard

--- src/components/home/BulletCard.tsx
+++ src/components/home/BulletCard.tsx
@@ -1,9 +1,47 @@
+import { Link } from 'wouter';
 import '../../assets/home/bullet-card.css';
+import Button from 'react-bootstrap/Button';
+import { Nav } from 'react-bootstrap';
 
-const MainCards = () => {      
+type Props = {
+    image: string,
+    title: string,
+    text: string,
+    route: string
+};
+
+const MainCards = ({image, title, text, route}: Props) => { 
+    const limitedText = text.length > 500 ? `${text.substring(0, 500)}...` : text;
+
+    const getImageUrl = () => {        
+        try {      
+            return `/src/assets/home/images/${image}`;
+            //return new URL(`/src/assets/home/images/${image}`, import.meta.url)
+        }
+        catch {
+            return '';
+        }
+    }
+    
     return (
-        <div className="bc-container">
-            Bullet Card
+        <div className="bc-container">            
+            <div className="bc-image-container">
+                <img 
+                    src={getImageUrl()}
+                    className="bc-image"                      
+                    alt="" />
+            </div>
+            <div className="bc-title pt-4">
+                {title}
+            </div>
+            <div className="bc-text px-4 py-4">
+                {limitedText}
+            </div>
+            <div className="bc-link-container">                
+                <Link href={route}>
+                    <Nav.Link>Czytaj dalej {'>'}</Nav.Link>
+                </Link>
+            </div>             
         </div>
     );
 }
--- src/components/home/MainCards.tsx
+++ src/components/home/MainCards.tsx
@@ -3,44 +3,38 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import BulletCard from './BulletCard';
 import '../../assets/home/main-cards.css';
-import { useEffect, useRef } from 'react';
 
 const MainCards = () => {   
-    
-
     return (
         <div className="mc-main-container">            
             <Container className="mc-container">
-                <Row no-gutters>
+                <Row>
                     <Col
                         cols="12"
                         md="4">                                                    
                         <BulletCard
-                            imagePath="cards/projects.jpg"
+                            image="card_projects.jpg"
                             title="Projekty"
                             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim feugiat eros a eleifend. Praesent tincidunt lacus ornare, efficitur sem sed, fringilla odio. Ut sed euismod nunc. Mauris dictum imperdiet nunc, nec sagittis felis rutrum sit amet. Nunc rhoncus quam volutpat neque gravida, consequat pulvinar felis blandit. In nulla turpis, laoreet non auctor eget, congue non mi. Morbi id dapibus massa, eu venenatis est. Quisque porta sem sed turpis euismod, eu scelerisque diam ultrices. Aenean et arcu ut erat mollis blandit id non magna. Quisque id congue risus, vel cursus orci.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim feugiat eros a eleifend. Praesent tincidunt lacus ornare, efficitur sem sed, fringilla odio. Ut sed euismod nunc. Mauris dictum imperdiet nunc, nec sagittis felis rutrum sit amet. Nunc rhoncus quam volutpat neque gravida, consequat pulvinar felis blandit. In nulla turpis, laoreet non auctor eget, congue non mi. Morbi id dapibus massa, eu venenatis est. Quisque porta sem sed turpis euismod, eu scelerisque diam ultrices. Aenean et arcu ut erat mollis blandit id non magna. Quisque id congue risus, vel cursus orci."
-                            route="solutions"
-                            class="ma-3"/>
+                            route="/solutions"/>
                     </Col>
                     <Col
                         cols="12"
                         md="4">                            
                         <BulletCard 
-                            imagePath="cards/services.jpg"
+                            image="card_services.jpg"
                             title="Usługi"
                             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim feugiat eros a eleifend. Praesent tincidunt lacus ornare, efficitur sem sed, fringilla odio. Ut sed euismod nunc. Mauris dictum imperdiet nunc, nec sagittis felis rutrum sit amet. Nunc rhoncus quam volutpat neque gravida, consequat pulvinar felis blandit. In nulla turpis, laoreet non auctor eget, congue non mi. Morbi id dapibus massa, eu venenatis est. Quisque porta sem sed turpis euismod, eu scelerisque diam ultrices. Aenean et arcu ut erat mollis blandit id non magna. Quisque id congue risus, vel cursus orci."
-                            route="services"
-                            class="ma-3"/>
+                            route="/services"/>
                     </Col>
                     <Col
                         cols="12"
                         md="4">                            
                         <BulletCard
-                            imagePath="cards/service.jpg"
+                            image="card_maintenance.jpg"
                             title="Serwis"
                             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim feugiat eros a eleifend. Praesent tincidunt lacus ornare, efficitur sem sed, fringilla odio. Ut sed euismod nunc. Mauris dictum imperdiet nunc, nec sagittis felis rutrum sit amet. Nunc rhoncus quam volutpat neque gravida, consequat pulvinar felis blandit. In nulla turpis, laoreet non auctor eget, congue non mi. Morbi id dapibus massa, eu venenatis est. Quisque porta sem sed turpis euismod, eu scelerisque diam ultrices. Aenean et arcu ut erat mollis blandit id non magna. Quisque id congue risus, vel cursus orci."
-                            route="service"
-                            class="ma-3"/>
+                            route="maintenance"/>
                     </Col>
                 </Row>
             </Container>
